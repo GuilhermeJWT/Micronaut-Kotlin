1baa4061ce43e58b8fe41d63d513ff21578252fa
Guilherme Santos <guiromanno@gmail.com>
2021-08-03 13:29:31 -0300

    Validando os dados com Bean Validation

--- src/main/kotlin/br/com/systemsgs/controller/CadastroAutorController.kt
+++ src/main/kotlin/br/com/systemsgs/controller/CadastroAutorController.kt
@@ -1,7 +1,19 @@
 package br.com.systemsgs.controller
 
+import br.com.systemsgs.dto.NovoAutorRequest
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/api/autores")
 class CadastroAutorController {
+
+    @Post
+    fun cadastra(@Body @Valid request: NovoAutorRequest){
+        println(request)
+    }
+
 }
--- src/main/kotlin/br/com/systemsgs/dto/NovoAutorRequest.kt
+++ src/main/kotlin/br/com/systemsgs/dto/NovoAutorRequest.kt
@@ -1,7 +1,21 @@
 package br.com.systemsgs.dto
 
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@Introspected
 data class NovoAutorRequest (
+    
+    @field:NotBlank(message = "O Nome deve ser Informado!")
     val nome: String,
+
+    @field:NotBlank(message = "E-Mail deve ser Informado!")
+    @field:Email(message = "Formato de E-Mail Inválido!")
     val email: String,
+
+    @field:NotBlank(message = "A Descrição deve ser Informada!")
+    @field:Size(max = 400, message = "Descrição deve ter no Máximo 400 Caracteres")
     val descricao: String
 )
