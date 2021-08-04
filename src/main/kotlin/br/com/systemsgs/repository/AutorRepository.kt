6e0983cbfbaeeb91d0919a68a8273b1622aa5012
Guilherme Santos <guiromanno@gmail.com>
2021-08-04 12:22:03 -0300

    Criando alguns métodos de Pesquisa com JPQL

--- src/main/kotlin/br/com/systemsgs/repository/AutorRepository.kt
+++ src/main/kotlin/br/com/systemsgs/repository/AutorRepository.kt
@@ -1,6 +1,7 @@
 package br.com.systemsgs.repository
 
 import br.com.systemsgs.model.ModelAutor
+import io.micronaut.data.annotation.Query
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
 import java.util.*
@@ -9,5 +10,14 @@ import java.util.*
 interface AutorRepository : JpaRepository<ModelAutor, Long>{
 
     fun findByEmail(email: String) : Optional<ModelAutor>
+
+    @Query("SELECT a FROM ModelAutor a WHERE a.email = :email")
+    fun buscaPorEmail(email: String) : Optional<ModelAutor>
+
+    @Query("SELECT a FROM ModelAutor a WHERE a.nome = :nome")
+    fun buscaPorNome(nome: String) : Optional<ModelAutor>
+
+    @Query("SELECT a FROM ModelAutor a WHERE a.descricao = :descricao")
+    fun buscaPorDescricao(descricao: String) : Optional<ModelAutor>
 
 }
